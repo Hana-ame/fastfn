6dbe74c2faa6c9bcf77ee8d765a75801b85347d5
lumino <luminovoez@gmail.com>
2026-04-02 21:44:12 +0800

    move consts

---
+++ consts.py
@@ -0,0 +1,21 @@
+# consts.py
+
+from pathlib import Path
+
+BASE_DIR = Path("functions")
+BASE_DIR.mkdir(exist_ok=True)
+
+def deep_equal(a, b):
+    if a == b:
+        return True
+    if type(a) != type(b):
+        return False
+    if isinstance(a, dict):
+        if set(a.keys()) != set(b.keys()):
+            return False
+        return all(deep_equal(a[k], b[k]) for k in a)
+    if isinstance(a, (list, tuple)):
+        if len(a) != len(b):
+            return False
+        return all(deep_equal(x, y) for x, y in zip(a, b))
+    return False
